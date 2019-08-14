9f68696702ab44be597dd2afeebd7be8d12daddd
dragazo <37459229+dragazo@users.noreply.github.com>
2019-08-13 21:45:30 -0500

    initial setup - runs the mines and the crafts

---
+++ src/main/java/com/atomicferret/thaumicdimensions/ThaumicDimensions.java
@@ -0,0 +1,38 @@
+package com.atomicferret.thaumicdimensions;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = ThaumicDimensions.MODID, name = ThaumicDimensions.NAME, version = ThaumicDimensions.VERSION)
+public class ThaumicDimensions
+{
+    public static final String MODID = "thaumicdimensions";
+    public static final String NAME = "Thaumic Dimensions";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+        logger.info("POST INIT");
+    }
+}
